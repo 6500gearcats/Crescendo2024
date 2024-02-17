193e5588b4e55e9de094ba80fbe201b02b0beb56
Katherine Avery <wearthrew1234@gmail.com>
2024-02-17 09:46:02 -0500

    Added new subsystems for neck and edited stuff

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -3,15 +3,35 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.FaultID;
+import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.utility.EncoderOdometer;
+
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
-  public Climber() {}
+  public Climber() {
+    public final CANSparkMax kRight_ClimberMotor = new CANSparkMax(ClimberConstants.kRight_ClimberMotorPort,MotorType.brushless);
+    m_lowerLimit = m_winchMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+    m_upperLimit = m_winchMotor.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public boolean ArmIsFullyExtended() {
+    boolean lowerLimit = m_lowerLimit.isPressed();
+    boolean upperLimit = m_upperLimit.isPressed();
+    SmartDashboard.putBoolean("Upper limit", upperLimit);
+    SmartDashboard.putBoolean("Lower limit", lowerLimit);
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,14 +4,57 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+// import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
+  private CANSparkMax m_sparkMax = new CANSparkMax(ShooterConstants.kShooterMotorPort, MotorType.kBrushless);
+
+  public final MotorController m_ShooterMotor = m_sparkMax;
+
+  private boolean m_bRingFired = false; 
+  private boolean m_bShooterAtSpeed = false;
+
+  private final RelativeEncoder m_shooterEncoder = m_sparkMax.getEncoder();
+  
   public Shooter() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Motor rotation", m_shooterEncoder.getVelocity());
+
   }
+
+  public void shooterSpeed() {
+    return m_ShooterMoter.get();
+
+  }
+
+  public void setShooterSpeedFast(){
+    m_ShooterMotor.set(ShooterConstants.kShooterSpeedFast);
+  }
+
+  public boolean shooterSpeedSetFast(){
+    m_bShooterAtSpeed = m_shooterEncoder.getVelocity() >= ShooterConstants.kShooterFastRPM;
+    return m_bShooterAtSpeed;
+  }
+  
+  public void reverseMotor() {
+    m_ShooterMotor.set(-ShooterConstants.kShooterSpeedSlow);
+  }
+
 }
+
+
+
+
+//shoot speaker, shoot, note in, speed up moter
