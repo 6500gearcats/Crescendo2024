9dd0146342f599e4497a0be9571ecafcbd0a01b8
Katherine Avery <wearthrew1234@gmail.com>
2024-02-21 17:19:18 -0500

    Pickupnote change

--- src/main/java/frc/robot/commands/PickUpNote.java
+++ src/main/java/frc/robot/commands/PickUpNote.java
@@ -38,7 +38,7 @@ public class PickUpNote extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_IntakeSystem.NoteIsPresent();
+    return !m_IntakeSystem.NoteIsPresent();
   }
 
   
