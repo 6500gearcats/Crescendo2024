11642d3eadc4d5b0a4479e59892cc04d2b72ccd6
Seth Bragdon <sethbragdon1@gmail.com>
2024-02-25 15:17:24 -0500

    Fixed neck logic

--- src/main/java/frc/robot/commands/NeckStable.java
+++ src/main/java/frc/robot/commands/NeckStable.java
@@ -24,7 +24,7 @@ public class NeckStable extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(!(m_Neck.getNeckAngle() > NeckConstants.KEncoderDeadbandThreshold))
+    if((m_Neck.getNeckAngle() > NeckConstants.KEncoderDeadbandThreshold))
     {
       m_Neck.getMotorController().set(NeckConstants.kNeckForwardSpeed);
     }
