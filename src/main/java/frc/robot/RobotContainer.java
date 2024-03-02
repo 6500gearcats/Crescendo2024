ed42e049374265adaa44947b4a2497d9b9b7f248
RobGinNC <robagale@aol.com>
2024-03-02 16:15:36 -0500

    Make rotation deadband bigger

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -85,7 +85,7 @@ private final Neck m_Neck = new Neck();
             () -> m_robotDrive.drive(
                 MathUtil.applyDeadband(-m_driverController.getLeftY(), 0.1), //0.1
                 MathUtil.applyDeadband(-m_driverController.getLeftX(), 0.1), //0.1
-                MathUtil.applyDeadband(-m_driverController.getRightX(), 0.1),
+                MathUtil.applyDeadband(-m_driverController.getRightX(), 0.2),
                 !m_driverController.getRightBumper()),
             m_robotDrive));
       
