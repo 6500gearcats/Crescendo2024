ff12454547e53765ae37964e5df04f8e9733aba5
Jason Holt <you@example.com>
2024-03-02 16:12:04 -0500

    Changed PID Values (Doesnt do anything) to 10 and added wheel base inches by .5

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,9 +49,9 @@ public final class Constants {
     public static final double kTurboModeModifier = 7.0/2.0;
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(28.0);
+    public static final double kTrackWidth = Units.inchesToMeters(28.5);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(23.0);
+    public static final double kWheelBase = Units.inchesToMeters(23.5);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
