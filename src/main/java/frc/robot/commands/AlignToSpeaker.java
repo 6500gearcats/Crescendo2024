5dbc3dc78cb0bfe5609dce90243487d2a44631a3
Seth Bragdon <sethbragdon1@gmail.com>
2024-03-17 14:53:16 -0400

    Code that would work with a better camera.

--- src/main/java/frc/robot/commands/AlignToSpeaker.java
+++ src/main/java/frc/robot/commands/AlignToSpeaker.java
@@ -13,47 +13,71 @@ import frc.robot.subsystems.DriveSubsystem;
 public class AlignToSpeaker extends Command {
   Vision m_vision;
   DriveSubsystem m_drive;
+  double rotation;
+  double d = 0;
   /** Creates a new AlignToSpeaker. */
   public AlignToSpeaker(Vision theVision, DriveSubsystem theDrive) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_vision = theVision;
     m_drive = theDrive;
     addRequirements(m_drive);
+    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    var alliance = DriverStation.getAlliance();
+
+    if(alliance.get() == DriverStation.Alliance.Red)
+    {
+      rotation = m_vision.getChosenTargetRotation(3);
+    }
+    else
+    {
+      rotation = m_vision.getChosenTargetDistance(7);
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double rotation = 0;
+    
     var alliance = DriverStation.getAlliance();
 
     double bSquared;
-    double a = -11;
+    double a = -9;
     double b;
     double c;
 
     if(alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red)
     {
       // Returns degrees
-      rotation = m_vision.getChosenTargetRotation(3);
+      if(m_vision.getChosenTargetRotation(3) != 0)
+      {
+        rotation = m_vision.getChosenTargetRotation(3);
+      }
+      
       c = m_vision.getChosenTargetDistance(3);
       bSquared = Math.pow(a, 2) + Math.pow(c, 2) -2 * (c*a) * Math.cos(rotation + 90);
       b = Math.sqrt(bSquared);
-      rotation -= Math.asin((Math.sin(rotation + 90) *a)/b);
+      d = Math.asin((Math.sin(rotation + 90) *a)/b);
+      rotation -= d;
     }
     else
     {
       // Returns degrees
       
-      rotation = m_vision.getChosenTargetRotation(3);
+      if(m_vision.getChosenTargetRotation(7) != 0)
+      {
+        rotation = m_vision.getChosenTargetRotation(7);
+      }
+
       c = m_vision.getChosenTargetDistance(7);
       bSquared = Math.pow(a, 2) + Math.pow(c, 2) -2 * (c*a) * Math.cos(rotation + 90);
       b = Math.sqrt(bSquared);
-      rotation -= Math.asin((Math.sin(rotation + 90) *a)/b);
+      d = Math.asin((Math.sin(rotation + 90) *a)/b);
+      rotation -= d;
     }
 
     if(Math.abs(rotation) > .1)
@@ -65,7 +89,7 @@ public class AlignToSpeaker extends Command {
       rotation = 0;
     }
 
-    m_drive.drive(0, 0, -rotation * .05, false);
+    m_drive.drive(0, 0, -rotation * .01, false);
   
   }
 
@@ -78,15 +102,7 @@ public class AlignToSpeaker extends Command {
   public boolean isFinished() {
     var alliance = DriverStation.getAlliance();
 
-    // Align to red or blue amp based on alliance
-    if(alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red) 
-    {
-      return m_vision.getChosenTargetRotation(3) == 0;
-    }
-    else
-    {
-      return m_vision.getChosenTargetRotation(7) == 0;
-    }
+    return Math.abs(rotation + d) < .1;
 
   }
 }
