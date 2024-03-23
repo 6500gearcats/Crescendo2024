ee9baae2069a924b21eb6c06a8a344d426981a11
Nicholas Rivera <nicriver0501@gmail.com>
2024-03-23 10:15:11 -0400

    Fixed Shooter Speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -230,9 +230,9 @@ public final class Constants {
 
       // NOT UPDATED TO 2024 SHOOTER VALUES (besides port)
       public static final double kShooterSpeedSlow = 0.6;
-      public static final double kShooterFastRPM = 2500;
+      public static final double kShooterFastRPM = 2000;
       public static final int kShooterSlowRPM = 3000;
-      public static final double kShooterSpeedFast = -0.75;
+      public static final double kShooterSpeedFast = -0.6;
       public static final double kShooterReverseFast = 0.9;
       public static final double kShooterSpeed = 1.1; //0.43
       public static final double kBallFiredThreshold = 0.1;
